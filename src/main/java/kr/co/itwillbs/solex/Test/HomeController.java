ca78a5835da2dfecb1d88c77cdde8054b3b87b25
jyeong11~ <bjy0911@naver.com>
2025-05-30 16:03:27 +0900

    기본설정

--- src/main/java/kr/co/itwillbs/solex/Test/HomeController.java
+++ src/main/java/kr/co/itwillbs/solex/Test/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class TestController {
+public class HomeController {
 	
 	@GetMapping("")
-	public String TestView() {
-		return "test";
+	public String home() {
+		return "main";
 	}
 	
 }
