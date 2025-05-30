e260641920d9cafac3624d9fc1807f88d984f9e0
Song-Mins <130220205+Song-Mins@users.noreply.github.com>
2025-05-31 00:38:43 +0900

    Initial commit

---
+++ src/main/java/kr/co/itwillbs/solex/SolexApplication.java
@@ -0,0 +1,13 @@
+package kr.co.itwillbs.solex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/itwillbs/solex/SolexApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.itwillbs.solex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
