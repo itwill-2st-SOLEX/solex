9c193934351de115d89e771ce13548ac6c762772
developerkjm <60417506+developerkjm@users.noreply.github.com>
2025-05-30 17:01:29 +0900

    get mapping

--- src/main/java/kr/co/itwillbs/solex/attendance/AttendanceController.java
+++ src/main/java/kr/co/itwillbs/solex/attendance/AttendanceController.java
@@ -1,14 +1,16 @@
 package kr.co.itwillbs.solex.attendance;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 @RequestMapping("/attendance")
 public class AttendanceController {
 	@GetMapping("/my_attend")
 	public String getMyAttend() {
-		return "/commute/my_attend";
+		System.out.println("출퇴근 현호아 나오나?? asldkfj;alkejf;lwaeijlkj");
+		return "/attendance/my_attend";
 	}
 }
 
