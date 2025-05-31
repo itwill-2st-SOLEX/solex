ac88184f94225f68d208e7dfbe92fd146d533635
jmkang <jmkang1984>
2025-05-31 18:52:07 +0900

    table editing

--- src/main/java/kr/co/itwillbs/solex/attendance/AttendanceController.java
+++ src/main/java/kr/co/itwillbs/solex/attendance/AttendanceController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AttendanceController {
 	@GetMapping("/my_attend")
 	public String getMyAttend() {
-		System.out.println("출퇴근 현호아 나오나?? asldkfj;alkejf;lwaeijlkj");
 		return "/attendance/my_attend";
 	}
 }
