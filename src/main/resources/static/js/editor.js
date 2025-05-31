6930147302ba3a934d2411ca81840162f5a867dd
LiSyu06 <104485335+LiSyu06@users.noreply.github.com>
2025-05-31 18:01:58 +0900

    1. 실제 내용 표시 부분 기본 css 설정 2. 로고 표시되도록 수정 3. 그리드 기본 설정 4. 에디터 사진, 표 입력 금지 설정

---
+++ src/main/resources/static/js/editor.js
@@ -0,0 +1,18 @@
+
+const editor = new toastui.Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'WYSIWYG',
+    previewStyle: 'vertical',
+    toolbarItems: [
+      ['heading', 'bold', 'italic'],
+      ['ul', 'ol']
+    ],
+	hideModeSwitch: true,                   // 모드 전환 탭 숨김
+    hooks: {
+      addImageBlobHook: () => {
+        alert('이미지는 업로드할 수 없습니다.');
+        return false;
+      }
+    }
+  });
