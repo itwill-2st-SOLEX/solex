d5686a229cc05fba23769871f8d3c53c937eddaf
seokh17 <sk000201@naver.com>
2025-06-09 11:03:28 +0900

    조직도 준비중

--- src/main/java/kr/co/itwillbs/solex/MainController.java
+++ src/main/java/kr/co/itwillbs/solex/MainController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("SOLEX")
 public class MainController {
 	
 	@GetMapping("")
