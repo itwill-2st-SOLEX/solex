f8e5fd06831571d95595c250161393cd8f428da2
jyeong11~ <bjy0911@naver.com>
2025-06-16 11:33:05 +0900

    feat: 로그인 개발중

--- src/main/java/kr/co/itwillbs/solex/login/LoginService.java
+++ src/main/java/kr/co/itwillbs/solex/login/LoginService.java
@@ -1,5 +1,7 @@
 package kr.co.itwillbs.solex.login;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +9,14 @@ import org.springframework.stereotype.Service;
 public class LoginService {
 	@Autowired
 	LoginMapper mapper;
+
+	public Map<String, String> loginEmp(String emp_num, String emp_pw) {
+		return mapper.loginEmp(emp_num, emp_pw);
+	}
+	
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		
+//		return null;
+//	}
 }
