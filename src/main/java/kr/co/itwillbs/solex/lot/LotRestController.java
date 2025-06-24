f65ed07769c96484f51c93415e5c1faf23967e29
Rempard17 <sk000201@naver.com>
2025-06-24 19:04:58 +0900

    LOT 화면 DB설계 및 조회준비

--- src/main/java/kr/co/itwillbs/solex/lot/LotRestController.java
+++ src/main/java/kr/co/itwillbs/solex/lot/LotRestController.java
@@ -1,8 +1,28 @@
 package kr.co.itwillbs.solex.lot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class LotRestController {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("lot")
+public class LotRestController {
+	
+	@Autowired
+	private LotService lotService;
+
+	@GetMapping("list")
+	public Map<String, Object> getLotList() {
+		
+		Map<String, Object> list = new HashMap<>();
+		
+		return list;
+	}
+	
+	
 }
--- src/main/resources/static/js/lot/lot.js
+++ src/main/resources/static/js/lot/lot.js
@@ -1,3 +1,59 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
+
+	$(function() {
+		$('#lotTree').jstree({
+		  'core': {
+		    'data': [
+		      {
+		        "text": "LOT-20250624-W19-P001-10",
+		        "icon": "fa fa-cog",
+		        "children": [ 
+					{ 
+						"text": "LOT-20250624-CUTTING-",
+						"icon": "fa fa-cog",
+						"children": [
+							{ "text": "A09깔창" },
+							{ "text": "원단7B" }
+						] 
+					},
+					{ "text": "봉재", "icon": "fa fa-cog" },
+					{ "text": "성형", "icon": "fa fa-cog" },
+					{ "text": "접착", "icon": "fa fa-cog" },
+					{ "text": "조립", "icon": "fa fa-cog" },
+					{ "text": "스탬핑", "icon": "fa fa-cog" },
+					{ "text": "마감", "icon": "fa fa-cog" }
+				 ]
+		      },
+		      {
+		        "text": "20250416-W4-Q093-1 - 스니커즈",
+		        "icon": "fa fa-scissors",
+				"children": [
+					{
+						"text": "재단",
+						"icon": "fa fa-cog",
+						"children" : [
+							{ "text": "CUTTING" },
+							{ "text": "2025-06-24 ~ 2025-06-25" },
+							{ "text": "서강현 / 75" },
+							{ "text": "인조가죽 오버레이 / 1500" },
+							{ "text": "설비-005A" },
+							{ "text": "생산수량 : 1500, 불량수량 : 32" },
+							{ "text": "생산완료" }
+						]
+					},
+					{ "text": "봉제", "icon": "fa fa-cog" },
+					{ "text": "성형", "icon": "fa fa-cog" },
+					{ "text": "접착", "icon": "fa fa-cog" },
+					{ "text": "안감/장식부착", "icon": "fa fa-cog" },
+					{ "text": "조립", "icon": "fa fa-cog" },
+					{ "text": "스탬핑", "icon": "fa fa-cog" },
+					{ "text": "자수", "icon": "fa fa-cog" },
+					{ "text": "마감", "icon": "fa fa-cog" },
+				]
+		      }
+		    ]
+		  }
+		});
+	});
 	
 });
