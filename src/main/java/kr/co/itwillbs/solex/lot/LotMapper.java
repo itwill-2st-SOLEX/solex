d1c377294cffb849f284cc9d84adfafed8f60a11
Rempard17 <sk000201@naver.com>
2025-06-24 10:39:50 +0900

    LOT 초기 화면구성

---
+++ src/main/java/kr/co/itwillbs/solex/lot/LotMapper.java
@@ -0,0 +1,8 @@
+package kr.co.itwillbs.solex.lot;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LotMapper {
+
+}
---
+++ src/main/java/kr/co/itwillbs/solex/lot/LotService.java
@@ -0,0 +1,8 @@
+package kr.co.itwillbs.solex.lot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LotService {
+
+}
