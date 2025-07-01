f18a08561997ff7a6a4b44d9cbe38e59cce8126f
jyouna <yubbi0516@gmail.com>
2025-07-01 17:25:11 +0900

    몰래 고치기 ㅋ

--- src/main/java/kr/co/itwillbs/solex/equipment_history/EquipmentHistoryRestController.java
+++ src/main/java/kr/co/itwillbs/solex/equipment_history/EquipmentHistoryRestController.java
@@ -1,13 +1,26 @@
 package kr.co.itwillbs.solex.equipment_history;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/equipment_his")
+@RequestMapping("/equipment_history")
 public class EquipmentHistoryRestController {
 	
 	@Autowired
 	private EquipmentHistoryService equipmentHistoryService;
+	
+	@GetMapping("") // 
+	public List<Map<String, Object>> getEquipmentHistory(@RequestParam("page") int page, @RequestParam("size") int size) throws Exception {
+        
+		int offset = page * size;
+		
+		return equipmentHistoryService.getEquipmentHistory(offset, size);
+    }
 }
