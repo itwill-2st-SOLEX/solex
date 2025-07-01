b0d230f0fa379a4fdea47cbaeabaa50155f28d03
jyouna <yubbi0516@gmail.com>
2025-07-01 18:09:52 +0900

    아진짜 하기 싫다

--- src/main/java/kr/co/itwillbs/solex/equipment_history/EquipmentHistoryRestController.java
+++ src/main/java/kr/co/itwillbs/solex/equipment_history/EquipmentHistoryRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/equipment_history")
+@RequestMapping("/equipmenthistory")
 public class EquipmentHistoryRestController {
 	
 	@Autowired
@@ -20,7 +20,15 @@ public class EquipmentHistoryRestController {
 	public List<Map<String, Object>> getEquipmentHistory(@RequestParam("page") int page, @RequestParam("size") int size) throws Exception {
         
 		int offset = page * size;
-		
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~page:" + page + " size: " + size + " offset = " + offset);
 		return equipmentHistoryService.getEquipmentHistory(offset, size);
     }
+	
+	//설비수리이력 목록조회
+	@GetMapping("/equipmenthistoryList")
+	public List<Map<String, Object>> equipmenthistoryList() {
+		
+		return equipmentHistoryService.equipmenthistoryList();
+	}
+	
 }
