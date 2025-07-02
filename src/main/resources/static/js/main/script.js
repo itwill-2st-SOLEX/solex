c88309105b337b351e156233ae45875626615bb7
LiSyu06 <whitesea85@naver.com>
2025-07-02 09:57:35 +0900

    임시저장 - 송민석

--- src/main/resources/static/js/main/script.js
+++ src/main/resources/static/js/main/script.js
@@ -1,4 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
+const calendar = new tui.Calendar('#calendar', {
+        defaultView   : 'month',
+        taskView      : true,
+        useFormPopup  : true,
+        useDetailPopup: true,
+        calendars     : [{ id:'todo', name:'To‑Do', backgroundColor:'#ffbb3b' }]
+        });
+
+        // 팝업에서 "저장" 눌렀을 때 호출됨
+        calendar.on('beforeCreateEvent', ({ title, start, end, isAllDay }) => {
+            // 달력에 바로 추가
+            calendar.createEvents([
+            {
+                id        : String(Date.now()), // 임시 ID
+                calendarId: 'todo',
+                title     : title,
+                start     : start,
+                end       : end,
+                isAllDay  : isAllDay,
+                category  : isAllDay ? 'allday' : 'time'
+            }
+            ]);
+        });
+
+        /* ── 디테일 팝업 → ‘수정’ 후 저장 ------------------------- */
+        calendar.on('beforeUpdateEvent', ({ event, changes }) => {
+        calendar.updateEvent(event.id, event.calendarId, changes);
+        });
+
+        /* ── 디테일 팝업 → ‘삭제’ ------------------------------- */
+        calendar.on('beforeDeleteEvent', ({ id, calendarId }) => {
+        calendar.deleteEvent(id, calendarId);
+        });
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
   const $ = (sel) => document.querySelector(sel);
 
   // 공지사항
@@ -107,3 +151,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.navbar--range').textContent = `${year}년 ${month}월`;
   }
 });
+*/
