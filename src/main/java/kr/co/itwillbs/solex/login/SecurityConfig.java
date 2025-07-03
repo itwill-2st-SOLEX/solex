1af72a22701597f437e68d09168ee35fcbc718e0
jyeong11~ <bjy0911@naver.com>
2025-07-03 12:21:36 +0900

    수정했습니당

--- src/main/java/kr/co/itwillbs/solex/login/SecurityConfig.java
+++ src/main/java/kr/co/itwillbs/solex/login/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
