1dd7ee06ec86d3e158bf8cd6b491e746d021a825
developerkjm <jmkang1984@gmail.com>
2025-07-07 12:23:39 +0900

    제품, bom 권한 설정 완료

--- src/main/java/kr/co/itwillbs/solex/products/ProductsRestController.java
+++ src/main/java/kr/co/itwillbs/solex/products/ProductsRestController.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
